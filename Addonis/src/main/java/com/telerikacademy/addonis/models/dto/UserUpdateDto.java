9ce577dbff4826aecb8003c47c5ffc015d7cb181
Gabriela <georgievagabriela507@gmail.com>
2021-11-22 12:42:54 +0200

    Add dto for update

---
+++ Addonis/src/main/java/com/telerikacademy/addonis/models/dto/UserUpdateDto.java
@@ -0,0 +1,54 @@
+package com.telerikacademy.addonis.models.dto;
+
+import com.telerikacademy.addonis.models.ValidPassword;
+
+import javax.validation.constraints.NotNull;
+
+public class UserUpdateDto {
+    @ValidPassword
+    private String password;
+
+    @NotNull(message = "First name can't be empty")
+    private String firstName;
+
+    @NotNull(message = "Last name can't be empty")
+    private String lastName;
+
+    @NotNull(message = "Email can't be empty")
+    private String email;
+
+    public UserUpdateDto() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
