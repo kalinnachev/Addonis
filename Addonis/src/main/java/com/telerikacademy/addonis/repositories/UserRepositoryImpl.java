c2ba6842f061d83263a828dbe16c8f138fac1098
Gabriela <georgievagabriela507@gmail.com>
2021-11-23 12:59:49 +0200

    Add search method in UserRepository and UserRepositoryImpl

--- Addonis/src/main/java/com/telerikacademy/addonis/repositories/UserRepositoryImpl.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/repositories/UserRepositoryImpl.java
@@ -5,8 +5,14 @@ import com.telerikacademy.addonis.models.User;
 import com.telerikacademy.addonis.repositories.contracts.UserRepository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 import static java.lang.String.format;
 
@@ -39,6 +45,38 @@ public class UserRepositoryImpl extends CRUDSQLRepository<User> implements UserR
                     .setParameter("value", email)
                     .uniqueResultOptional()
                     .orElseThrow(() -> new EntityNotFoundException("User", "email", email));
+        }
+    }
+
+    @Override
+    public List<User> search(Optional<String> username, Optional<String> email, Optional<String> phoneNumber) {
+        try (Session session = getSessionFactory().openSession()) {
+            var queryString = new StringBuilder(" from User ");
+            var search = new ArrayList<String>();
+            var params = new HashMap<String, Object>();
+
+            username.ifPresent(value -> {
+                search.add("username like :username");
+                params.put("username", "%" + username.get() + "%");
+            });
+
+            email.ifPresent(value -> {
+                search.add("email like :email");
+                params.put("email", "%" + email.get() + "%");
+            });
+
+            phoneNumber.ifPresent(value -> {
+                search.add("phoneNumber like :phoneNumber");
+                params.put("phoneNumber", "%" + phoneNumber.get() + "%");
+            });
+
+            if(!search.isEmpty()) {
+                queryString.append(" where ").append(String.join(" or ", search));
+            }
+            System.out.println(queryString);
+            Query<User> query = session.createQuery(queryString.toString(), User.class);
+            query.setProperties(params);
+            return query.list();
         }
     }
 }
--- Addonis/src/main/java/com/telerikacademy/addonis/repositories/contracts/UserRepository.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/repositories/contracts/UserRepository.java
@@ -2,9 +2,14 @@ package com.telerikacademy.addonis.repositories.contracts;
 
 import com.telerikacademy.addonis.models.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends CRUDRepository<User> {
 
     User findByUsername(String username);
 
     User findByEmail(String email);
+
+    List<User> search(Optional<String> username, Optional<String> email, Optional<String> phoneNumber);
 }
