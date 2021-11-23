ca1da7c4a1ce5a611b29ee62a8ba8fb00115a264
Gabriela <georgievagabriela507@gmail.com>
2021-11-23 13:00:35 +0200

    Add search,block and unblock methods in UserService and UserServiceImpl

--- Addonis/src/main/java/com/telerikacademy/addonis/services/contracts/UserService.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/services/contracts/UserService.java
@@ -3,6 +3,7 @@ package com.telerikacademy.addonis.services.contracts;
 import com.telerikacademy.addonis.models.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     User getById(int id);
@@ -14,4 +15,11 @@ public interface UserService {
     void delete(int id);
 
     void update(User user);
+
+    User block(int id);
+
+    User unblock(int id);
+
+    List<User> search(Optional<String> username, Optional<String> email, Optional<String> phoneNumber);
+
 }
