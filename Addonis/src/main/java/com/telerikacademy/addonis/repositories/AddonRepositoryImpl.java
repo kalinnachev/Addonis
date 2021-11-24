8bd04d4682e0c6b3cf04b7a0ad254679fe13d89a
Borislav Ignatov <borkata13@gmail.com>
2021-11-24 11:29:28 +0200

    Implement findByOriginUrl()

--- Addonis/src/main/java/com/telerikacademy/addonis/repositories/AddonRepositoryImpl.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/repositories/AddonRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.telerikacademy.addonis.repositories;
 
+import com.telerikacademy.addonis.exceptions.EntityNotFoundException;
 import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.repositories.contracts.AddonRepository;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import static java.lang.String.format;
 
 @Repository
 public class AddonRepositoryImpl extends CRUDSQLRepository<Addon> implements AddonRepository {
@@ -12,5 +16,17 @@ public class AddonRepositoryImpl extends CRUDSQLRepository<Addon> implements Add
     @Autowired
     protected AddonRepositoryImpl(SessionFactory sessionFactory) {
         super(sessionFactory, Addon.class);
+    }
+
+    @Override
+    public Addon findByOriginUrl(String originUrl) {
+        String query = format("from %s where %s = :value", getClazz().getSimpleName(), "originUrl");
+        try(Session session = getSessionFactory().openSession()){
+            return session
+                    .createQuery(query, getClazz())
+                    .setParameter("value", originUrl)
+                    .uniqueResultOptional()
+                    .orElseThrow(()-> new EntityNotFoundException("Addon", "originUrl", originUrl));
+        }
     }
 }
--- Addonis/src/main/java/com/telerikacademy/addonis/repositories/contracts/AddonRepository.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/repositories/contracts/AddonRepository.java
@@ -3,6 +3,6 @@ package com.telerikacademy.addonis.repositories.contracts;
 import com.telerikacademy.addonis.models.Addon;
 
 public interface AddonRepository extends CRUDRepository<Addon> {
-
+    Addon findByOriginUrl(String originUrl);
 
 }
