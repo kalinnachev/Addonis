f6a5ca16166aa043984bca0d5e4d9b7194bcda8a
kalinnachev <kalin.nachev@gmail.com>
2021-11-25 19:03:09 +0200

    Add @EnableScheduling annotation

--- Addonis/src/main/java/com/telerikacademy/addonis/config/RestConfig.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/config/RestConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableScheduling
 @PropertySource("classpath:application.properties")
 public class RestConfig {
 
