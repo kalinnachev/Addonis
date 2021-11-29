ed097f8a34fb477e182be798e5788ceb6376d103
Gabriela <georgievagabriela507@gmail.com>
2021-11-29 12:30:52 +0200

    Add new class UserPhoto

---
+++ Addonis/src/main/java/com/telerikacademy/addonis/models/UserPhoto.java
@@ -0,0 +1,9 @@
+package com.telerikacademy.addonis.models;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class UserPhoto {
+    private String name;
+    private MultipartFile document;
+}
+
