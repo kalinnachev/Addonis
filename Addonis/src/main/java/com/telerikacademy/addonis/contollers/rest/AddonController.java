c599e78d646978cb258d3e68dcd879a1684d448b
Borislav Ignatov <borkata13@gmail.com>
2021-12-01 15:21:00 +0200

    Implement checkIfAdmin() and checkIfBlocked()

--- Addonis/src/main/java/com/telerikacademy/addonis/contollers/rest/AddonController.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/contollers/rest/AddonController.java
@@ -9,7 +9,9 @@ import com.telerikacademy.addonis.services.contracts.UserService;
 import com.telerikacademy.addonis.untilities.AuthenticationHelper;
 import com.telerikacademy.addonis.untilities.ModelMapperAddon;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -17,9 +19,10 @@ import java.util.List;
 @RestController
 @RequestMapping("api/addons")
 public class AddonController {
+    public static final String ERROR_MSG_BLOCKED = "Action is blocked";
+    public static final String ERROR_MSG_ADMIN = "User must be admin";
     private final ModelMapperAddon modelMapperAddon;
     private final AddonService addonService;
-    private final UserService userService;
     private final AuthenticationHelper authenticationHelper;
 
     public AddonController(ModelMapperAddon modelMapperAddon, AddonService addonService, UserService userService, AuthenticationHelper authenticationHelper) {
@@ -67,7 +70,6 @@ public class AddonController {
         return addon;
     }
 
-    //TODO when Dto is done
     @PutMapping("/{id}")
     public Addon updateAddon(@PathVariable int id,
                              @Valid @RequestBody AddonUpdateDto addonUpdateDto) {
@@ -92,5 +94,17 @@ public class AddonController {
     public void deleteAddon(@PathVariable int id) {
         addonService.delete(id);
 
+    }
+
+    private void checkIfAdmin(User user) {
+        if(!user.isAdmin()){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, ERROR_MSG_ADMIN);
+        }
+    }
+
+    private void checkIfBlocked(User user) {
+        if(!user.isBlocked()){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, ERROR_MSG_BLOCKED);
+        }
     }
 }
