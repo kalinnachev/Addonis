cbd2fb77f14aba1fd8f51cdf446b1ae470591126
kalinnachev <kalin.nachev@gmail.com>
2021-12-01 13:13:43 +0200

    Refactor - use method transferTo

--- Addonis/src/main/java/com/telerikacademy/addonis/untilities/IOUtils.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/untilities/IOUtils.java
@@ -3,18 +3,13 @@ package com.telerikacademy.addonis.untilities;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class IOUtils {
 
-    public static File convert(MultipartFile file) throws IOException {
-        String tmpDirsLocation = System.getProperty("java.io.tmpdir");
-        File convFile = new File(tmpDirsLocation + file.getOriginalFilename());
-        convFile.createNewFile();
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
+    public static File convert(MultipartFile multipartFile) throws IOException {
+        File file = new File(System.getProperty("java.io.tmpdir") + multipartFile.getOriginalFilename());
+        multipartFile.transferTo(file);
+        return file;
     }
 }
