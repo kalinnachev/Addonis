e4f785a21acc07bee3a8e018923f3a342bfec339
kalinnachev <kalin.nachev@gmail.com>
2021-12-02 21:08:07 +0200

    Add new column - logo in TargetIde

--- Addonis/src/main/java/com/telerikacademy/addonis/models/TargetIde.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/models/TargetIde.java
@@ -10,9 +10,11 @@ public class TargetIde {
     @Column(name = "id")
     private Integer id;
 
-
     @Column(name = "name")
     private String name;
+
+    @Column(name = "image_file_name")
+    private String logo;
 
     public Integer getId() {
         return id;
@@ -28,5 +30,13 @@ public class TargetIde {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
     }
 }
