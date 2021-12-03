4fb39ad6fd83d92453e81735b6ec6d1c058d4a07
kalinnachev <kalin.nachev@gmail.com>
2021-12-03 22:45:56 +0200

    New methods in view formatter

--- Addonis/src/main/java/com/telerikacademy/addonis/contollers/mvc/ViewFormatter.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/contollers/mvc/ViewFormatter.java
@@ -3,13 +3,16 @@ package com.telerikacademy.addonis.contollers.mvc;
 import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.models.TargetIde;
 import org.ocpsoft.prettytime.PrettyTime;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 public class ViewFormatter {
 
     PrettyTime prettyTime = new PrettyTime();
 
     public String getAverageRating(Addon addon){
-        return String.format("Average rating "+addon.getAverageRating());
+        if(addon.getAverageRating() == 0)
+            return "Not rated yet";
+        return String.format("Average rating "+addon.getAverageRating() + " out of 5");
     }
 
     public String getLogoSource(TargetIde ide){
@@ -18,6 +21,23 @@ public class ViewFormatter {
 
     public String getLastCommit(Addon addon){
         return "Last commit : " + prettyTime.format(addon.getRepoInfo().getLastCommitDate());
+    }
+
+    public int getNumberOfStars(Addon addon){
+        return (int) Math.round(addon.getAverageRating());
+    }
+
+    public String getNumberOfDownloads(Addon addon){
+
+        return addon.getNumberOfDownloads() + " downloads";
+    }
+
+    public String getAddonDownloadLink(Addon addon){
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .replacePath(
+                        String.format("api/storage/addons/%d/content", addon.getId()))
+                .build()
+                .toUriString();
     }
 
 }
