dda06afaf143de7715c65da4d588b4927bc716fd
Gabriela <georgievagabriela507@gmail.com>
2021-12-04 13:56:38 +0200

    Add new model attribute in showSingleAddon

--- Addonis/src/main/java/com/telerikacademy/addonis/contollers/mvc/AddonMVC.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/contollers/mvc/AddonMVC.java
@@ -3,12 +3,17 @@ package com.telerikacademy.addonis.contollers.mvc;
 
 import com.telerikacademy.addonis.exceptions.EntityNotFoundException;
 import com.telerikacademy.addonis.models.Addon;
+import com.telerikacademy.addonis.models.Tag;
 import com.telerikacademy.addonis.services.contracts.AddonService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -27,6 +32,9 @@ public class AddonMVC {
             Addon addon = addonService.getById(id);
             model.addAttribute("addon", addon);
             model.addAttribute("vf",new ViewFormatter());
+            model.addAttribute("addons",addonService.getAll());
+            model.addAttribute("addonsFilterByTargetIde", addonService.filter(Optional.empty(), Optional.of(addon.getTargetIde().getId())
+                    ,Optional.empty()));
             return "product";
         } catch (EntityNotFoundException e) {
             model.addAttribute("error", e.getMessage());
