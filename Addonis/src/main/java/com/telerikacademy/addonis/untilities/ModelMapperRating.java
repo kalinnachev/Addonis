f1d4d154944cb2bce92f3b12473c871aac2a1c49
Borislav Ignatov <borkata13@gmail.com>
2021-12-04 15:00:23 +0200

    Implement ModelMapperRating

---
+++ Addonis/src/main/java/com/telerikacademy/addonis/untilities/ModelMapperRating.java
@@ -0,0 +1,33 @@
+package com.telerikacademy.addonis.untilities;
+
+import com.telerikacademy.addonis.models.Addon;
+import com.telerikacademy.addonis.models.Rating;
+import com.telerikacademy.addonis.models.User;
+import com.telerikacademy.addonis.models.dto.RatingDto;
+import com.telerikacademy.addonis.services.contracts.AddonService;
+import com.telerikacademy.addonis.services.contracts.RatingService;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperRating {
+    private final RatingService ratingService;
+    private final AddonService addonService;
+
+    public ModelMapperRating(RatingService ratingService, AddonService addonService) {
+        this.ratingService = ratingService;
+        this.addonService = addonService;
+    }
+
+    public Rating fromDto(int id, RatingDto ratingDto, User user){
+        Rating rating = new Rating();
+        dtoToObject(ratingDto, user, rating,id);
+        return rating;
+    }
+
+    private void dtoToObject(RatingDto ratingDto, User user, Rating rating, int id) {
+        rating.setAddon(addonService.getById(id));
+        rating.setUser(user);
+        rating.setRating(ratingDto.getRating());
+    }
+
+}
