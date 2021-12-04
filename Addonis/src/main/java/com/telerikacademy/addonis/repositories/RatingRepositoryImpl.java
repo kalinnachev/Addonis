2ffd1386ce37c8237c0690e9d6c4912e1dfc1863
Borislav Ignatov <borkata13@gmail.com>
2021-12-04 16:31:10 +0200

    Implement getByUserAndAddon()

--- Addonis/src/main/java/com/telerikacademy/addonis/repositories/RatingRepositoryImpl.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/repositories/RatingRepositoryImpl.java
@@ -1,14 +1,33 @@
 package com.telerikacademy.addonis.repositories;
 
+import com.telerikacademy.addonis.exceptions.EntityNotFoundException;
+import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.models.Rating;
+import com.telerikacademy.addonis.models.User;
 import com.telerikacademy.addonis.repositories.contracts.RatingRepository;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+
+import static java.lang.String.format;
 
 @Repository
 public class RatingRepositoryImpl extends CRUDSQLRepository<Rating> implements RatingRepository {
 
     protected RatingRepositoryImpl(SessionFactory sessionFactory) {
         super(sessionFactory, Rating.class);
+    }
+
+    @Override
+    public Rating getByUserAndAddon(Addon addon, User user) {
+        String query = format("from %s where %s = :addon and %s = :user", getClazz().getSimpleName(), "addon", "user");
+        try (Session session = getSessionFactory().openSession()) {
+            return session
+                    .createQuery(query, getClazz())
+                    .setParameter("addon", addon)
+                    .setParameter("user",user)
+                    .uniqueResultOptional()
+                    .orElseThrow(() -> new EntityNotFoundException("You have not yet rated that addon"));
+        }
     }
 }
--- Addonis/src/main/java/com/telerikacademy/addonis/repositories/contracts/RatingRepository.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/repositories/contracts/RatingRepository.java
@@ -1,7 +1,10 @@
 package com.telerikacademy.addonis.repositories.contracts;
 
+import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.models.Rating;
+import com.telerikacademy.addonis.models.User;
 
 public interface RatingRepository extends CRUDRepository<Rating>{
 
+    Rating getByUserAndAddon(Addon addon, User user);
 }
