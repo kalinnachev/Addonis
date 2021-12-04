d0f0a047cbac32b44fd3106e2325e03ae79df899
kalinnachev <kalin.nachev@gmail.com>
2021-12-04 22:18:15 +0200

    Add new method getUserPictureLink

--- Addonis/src/main/java/com/telerikacademy/addonis/contollers/mvc/ViewFormatter.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/contollers/mvc/ViewFormatter.java
@@ -3,6 +3,7 @@ package com.telerikacademy.addonis.contollers.mvc;
 import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.models.Tag;
 import com.telerikacademy.addonis.models.TargetIde;
+import com.telerikacademy.addonis.models.User;
 import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -13,33 +14,41 @@ public class ViewFormatter {
 
     PrettyTime prettyTime = new PrettyTime();
 
-    public String getAverageRating(Addon addon){
-        if(addon.getAverageRating() == 0)
+    public String getAverageRating(Addon addon) {
+        if (addon.getAverageRating() == 0)
             return "Not rated yet";
-        return String.format("Average rating "+addon.getAverageRating() + " out of 5");
+        return String.format("Average rating " + addon.getAverageRating() + " out of 5");
     }
 
-    public String getLogoSource(TargetIde ide){
-        return "./img/"+ ide.getLogo();
+    public String getLogoSource(TargetIde ide) {
+        return "./img/" + ide.getLogo();
     }
 
-    public String getLastCommit(Addon addon){
+    public String getLastCommit(Addon addon) {
         return "Last commit : " + prettyTime.format(addon.getRepoInfo().getLastCommitDate());
     }
-    public String getLinkBinary(Addon addon){
-       return ServletUriComponentsBuilder.fromCurrentContextPath().
-                replacePath(String.format("./api/storage/addons/%d/content",addon.getId()))
+
+    public String getLinkBinary(Addon addon) {
+        return ServletUriComponentsBuilder.fromCurrentContextPath().
+                replacePath(String.format("/api/storage/addons/%d/content", addon.getId()))
                 .build().toString();
     }
-    public int getNumberOfStars(Addon addon){
+
+    public String getUserPictureLink(User user) {
+        return ServletUriComponentsBuilder.fromCurrentContextPath().
+                replacePath(String.format("/api/storage/users/%d/picture", user.getId()))
+                .build().toString();
+    }
+
+    public int getNumberOfStars(Addon addon) {
         return (int) Math.round(addon.getAverageRating());
     }
 
-    public String getNumberOfDownloads(Addon addon){
-
+    public String getNumberOfDownloads(Addon addon) {
         return addon.getNumberOfDownloads() + " downloads";
     }
-    public List<Tag> getTagsAddon(Addon addon){
+
+    public List<Tag> getTagsAddon(Addon addon) {
         List<Tag> allTags = new ArrayList<>(addon.getTags());
         allTags.addAll(allTags);
         return allTags;
