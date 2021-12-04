075affda8438634e1f1045f835650d4ff4111190
Borislav Ignatov <borkata13@gmail.com>
2021-12-04 16:31:29 +0200

    Implement getByUserAndAddon() and update()

--- Addonis/src/main/java/com/telerikacademy/addonis/services/RatingServiceImpl.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/services/RatingServiceImpl.java
@@ -1,6 +1,8 @@
 package com.telerikacademy.addonis.services;
 
+import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.models.Rating;
+import com.telerikacademy.addonis.models.User;
 import com.telerikacademy.addonis.repositories.contracts.RatingRepository;
 import com.telerikacademy.addonis.services.contracts.RatingService;
 import org.springframework.stereotype.Service;
@@ -21,5 +23,15 @@ public class RatingServiceImpl implements RatingService {
     @Override
     public Rating getById(int id) {
         return ratingRepository.getById(id);
+    }
+
+    @Override
+    public Rating getByUserAndAddon(Addon addon, User user) {
+        return ratingRepository.getByUserAndAddon(addon,user);
+    }
+
+    @Override
+    public void update(Rating ratingToUpdate) {
+         ratingRepository.update(ratingToUpdate);
     }
 }
--- Addonis/src/main/java/com/telerikacademy/addonis/services/contracts/RatingService.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/services/contracts/RatingService.java
@@ -1,9 +1,15 @@
 package com.telerikacademy.addonis.services.contracts;
 
+import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.models.Rating;
+import com.telerikacademy.addonis.models.User;
 
 public interface RatingService {
     void create(Rating rating);
 
     Rating getById(int id);
+
+    Rating getByUserAndAddon(Addon byId, User user);
+
+    void update(Rating ratingToUpdate);
 }
