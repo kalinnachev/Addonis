424c3aeee8e5777c969f557b16dac1ecc4be861a
Gabriela <georgievagabriela507@gmail.com>
2021-12-07 15:55:48 +0200

    Delete annotation size and validPassword

--- Addonis/src/main/java/com/telerikacademy/addonis/models/dto/LoginDto.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/models/dto/LoginDto.java
@@ -7,16 +7,10 @@ import javax.validation.constraints.Size;
 
 public class LoginDto {
 
-    public static final int MIN_USERNAME_LENGTH = 2;
-    public static final int MAX_USERNAME_LENGTH = 20;
-    public static final String USERNAME_ERR_MSG = "Username should be between 2 and 20 symbols";
-
     @NotEmpty
-    @Size(min = MIN_USERNAME_LENGTH, max = MAX_USERNAME_LENGTH, message = USERNAME_ERR_MSG)
     private String username;
 
     @NotEmpty
-    @ValidPassword
     private String password;
 
     public String getUsername() {
