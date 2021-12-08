7ef7034e17ec7c97be889fdf961b00b8a863e9ed
Borislav Ignatov <borkata13@gmail.com>
2021-12-08 14:37:05 +0200

    Implement TagServiceTest.java

---
+++ Addonis/src/test/java/com/telerikacademy/addonis/services/TagServiceTest.java
@@ -0,0 +1,27 @@
+package com.telerikacademy.addonis.services;
+
+import com.telerikacademy.addonis.repositories.contracts.TagRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TagServiceTest {
+
+    @Mock
+    TagRepository repository;
+
+    @InjectMocks
+    TagServiceImpl service;
+
+    @Test
+    public void getAll_should_callRepository() {
+        service.getAll();
+
+        Mockito.verify(repository, Mockito.times(1))
+                .getAll();
+    }
+}
