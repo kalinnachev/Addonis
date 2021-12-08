ea7694644c83f62ea9efca8e9f1e57edf919b572
kalinnachev <kalin.nachev@gmail.com>
2021-12-08 02:08:19 +0200

    Add new dto - for approval/rejections

---
+++ Addonis/src/main/java/com/telerikacademy/addonis/models/dto/AddonApprovalDto.java
@@ -0,0 +1,15 @@
+package com.telerikacademy.addonis.models.dto;
+
+
+public class AddonApprovalDto {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
