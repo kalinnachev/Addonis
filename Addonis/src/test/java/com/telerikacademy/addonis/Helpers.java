8a1a6f44f594d0e5c4e8cc8ee94e939abb6f25b4
Borislav Ignatov <borkata13@gmail.com>
2021-12-08 14:11:27 +0200

    Implement createMockTag()

--- Addonis/src/test/java/com/telerikacademy/addonis/Helpers.java
+++ Addonis/src/test/java/com/telerikacademy/addonis/Helpers.java
@@ -1,7 +1,12 @@
 package com.telerikacademy.addonis;
 
-import com.telerikacademy.addonis.models.Role;
-import com.telerikacademy.addonis.models.User;
+import com.telerikacademy.addonis.models.*;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Helpers {
 
@@ -26,4 +31,13 @@ public class Helpers {
         mockRole.setRoleName(role);
         return mockRole;
     }
+
+    private static Tag createMockTag() {
+        var mockTag = new Tag();
+        mockTag.setId(1);
+        mockTag.setName("mockTag");
+        return mockTag;
+    }
+
+
 }
