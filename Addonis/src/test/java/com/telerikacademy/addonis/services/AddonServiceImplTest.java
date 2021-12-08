b9024b10b0d02dbbe32de100dcb4186358a91463
Borislav Ignatov <borkata13@gmail.com>
2021-12-08 15:17:02 +0200

    Implement getById_should_callRepository()

--- Addonis/src/test/java/com/telerikacademy/addonis/services/AddonServiceImplTest.java
+++ Addonis/src/test/java/com/telerikacademy/addonis/services/AddonServiceImplTest.java
@@ -1,12 +1,21 @@
 package com.telerikacademy.addonis.services;
 
+import com.telerikacademy.addonis.Helpers;
+import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.repositories.contracts.AddonRepository;
 import com.telerikacademy.addonis.services.contracts.FileService;
 import com.telerikacademy.addonis.services.contracts.RepoInfoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static com.telerikacademy.addonis.Helpers.createMockAddon;
 
 @ExtendWith(MockitoExtension.class)
 public class AddonServiceImplTest {
@@ -22,6 +31,16 @@ public class AddonServiceImplTest {
 
     @InjectMocks
     AddonServiceImpl addonService;
+
+    @Test
+    public void getById_should_callRepository() {
+        Addon mockAddon = createMockAddon();
+
+        addonService.getById(mockAddon.getId());
+
+        Mockito.verify(addonRepository, Mockito.times(1))
+                .getById(mockAddon.getId());
+    }
 
 
 }
