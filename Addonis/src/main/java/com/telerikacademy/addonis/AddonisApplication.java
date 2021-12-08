9a2775d31207ac5c5073ad4dc68c88ccbd9bcc5a
kalinnachev <kalin.nachev@gmail.com>
2021-12-08 02:17:12 +0200

    Clean up main class

--- Addonis/src/main/java/com/telerikacademy/addonis/AddonisApplication.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/AddonisApplication.java
@@ -20,92 +20,11 @@ import java.time.LocalDate;
 
 @SpringBootApplication
 @EnableSwagger2
-public class AddonisApplication implements CommandLineRunner {
-
-    @Autowired
-    ApplicationContext applicationContext;
+public class AddonisApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(AddonisApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-
-
-        //VerificationTokenService service = applicationContext.getBean(VerificationTokenService.class);
-
-        //service.verificationToken("gg");
-        //  test();
-//
-//
-        //testToken();
-
-        //applicationContext.getBean(FileService.class);
-
-
-
-//        GitRepository gitRepository = applicationContext.getBean(GitRepository.class);
-//        AddonGitInfo info =null;
-//        for (int i = 0;i<10;i++) {
-//            System.out.println("i = " + i);
-//            info = gitRepository.getInfo("https://github.com/spring-projects/spring-boot");
-//        }
-
-
-    }
-
-    private void testSendMail() {
-        MailService mailService = applicationContext.getBean(MailService.class);
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setSubject("Hello from Spring Boot");
-        mailMessage.setText("This is the message body");
-
-        User user = new User();
-        user.setEmail("kalin.nachev@gmail.com");
-
-        mailService.sendMail(user, mailMessage);
-
-    }
-
-    private void testToken() {
-        UserRepository userRepository = applicationContext.getBean(UserRepository.class);
-        VerificationTokenRepository verificationTokenRepository = applicationContext.getBean(VerificationTokenRepository.class);
-
-        User user = userRepository.getById(1);
-        VerificationToken verificationToken = new VerificationToken();
-        verificationToken.setUser(user);
-        verificationToken.setToken("TEST");
-        verificationTokenRepository.create(verificationToken);
-
-
-    }
-    private void test(){
-
-        UserRepository userRepository = applicationContext.getBean(UserRepository.class);
-        User user = userRepository.getById(1);
-        TargetIdeRepository targetIdeRepository = applicationContext.getBean(TargetIdeRepository.class);
-        TargetIde ide = targetIdeRepository.getById(1);
-
-
-
-
-        Addon addon = new Addon();
-        addon.setCreator(user);
-        addon.setCreationDate(LocalDate.now());
-        //addon.setApproved(false);
-        addon.setTargetIde(ide);
-        addon.setBinaryContentUrl("test");
-        addon.setDescription("test");
-        addon.setName("name");
-        addon.setOriginUrl("https://github.com/spring-projects/spring-boot");
-
-//        RepoInfoService repoInfoService = applicationContext.getBean(RepoInfoService.class);
-//        RepoInfo info = repoInfoService.createInfoForAddon(addon);
-//        addon.setRepoInfo(info);
-
-//        AddonService addonService = applicationContext.getBean(AddonService.class);
-//        addonService.create(addon);
-    }
 
 }
