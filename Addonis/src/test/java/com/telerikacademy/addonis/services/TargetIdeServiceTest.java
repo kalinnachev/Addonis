793ae169ec3ca1cdb80bca3207f37e2dddac5adf
Borislav Ignatov <borkata13@gmail.com>
2021-12-08 14:33:19 +0200

    Implement TargetIdeServiceTest.java

---
+++ Addonis/src/test/java/com/telerikacademy/addonis/services/TargetIdeServiceTest.java
@@ -0,0 +1,51 @@
+package com.telerikacademy.addonis.services;
+
+import com.telerikacademy.addonis.Helpers;
+import com.telerikacademy.addonis.models.TargetIde;
+import com.telerikacademy.addonis.repositories.contracts.TargetIdeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.telerikacademy.addonis.Helpers.createMockTargetIde;
+
+@ExtendWith(MockitoExtension.class)
+public class TargetIdeServiceTest {
+
+    @Mock
+    TargetIdeRepository repository;
+
+    @InjectMocks
+    TargetIdeServiceImpl service;
+
+    @Test
+    public void getByName_should_callRepository() {
+        TargetIde mockTargetIde = createMockTargetIde();
+
+        service.getByName(mockTargetIde.getName());
+
+        Mockito.verify(repository, Mockito.times(1))
+                .getByName(mockTargetIde.getName());
+    }
+
+    @Test
+    public void getById_should_callRepository() {
+        TargetIde mockTargetIde = createMockTargetIde();
+
+        service.getById(mockTargetIde.getId());
+
+        Mockito.verify(repository, Mockito.times(1))
+                .getById(mockTargetIde.getId());
+    }
+
+    @Test
+    public void getAll_should_callRepository() {
+        service.getAll();
+
+        Mockito.verify(repository, Mockito.times(1))
+                .getAll();
+    }
+}
