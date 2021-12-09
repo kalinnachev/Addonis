e8ee0c3acca3e7c47befd6a28852949d27de5ec4
kalinnachev <kalin.nachev@gmail.com>
2021-12-09 14:12:38 +0200

    Refactor AddonDtoMvc to/form Addon

--- Addonis/src/main/java/com/telerikacademy/addonis/untilities/ModelMapperAddon.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/untilities/ModelMapperAddon.java
@@ -4,15 +4,22 @@ import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.models.Tag;
 import com.telerikacademy.addonis.models.User;
 import com.telerikacademy.addonis.models.dto.AddonDto;
+import com.telerikacademy.addonis.models.dto.AddonDtoMvc;
 import com.telerikacademy.addonis.models.dto.AddonUpdateDto;
 import com.telerikacademy.addonis.repositories.contracts.TagRepository;
 import com.telerikacademy.addonis.services.contracts.AddonService;
 import com.telerikacademy.addonis.services.contracts.TargetIdeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
 public class ModelMapperAddon {
@@ -20,6 +27,7 @@ public class ModelMapperAddon {
     private final TargetIdeService targetIdeService;
     private final TagRepository tagRepository;
 
+    @Autowired
     public ModelMapperAddon(AddonService addonService,
                             TargetIdeService targetIdeService, TagRepository tagRepository) {
         this.addonService = addonService;
@@ -27,15 +35,15 @@ public class ModelMapperAddon {
         this.tagRepository = tagRepository;
     }
 
-    public Addon fromDto(AddonDto addonDto, User user){
+    public Addon fromDto(AddonDto addonDto, User user) {
         Addon addon = new Addon();
-        dtoToObject(addon,addonDto,user);
+        dtoToObject(addon, addonDto, user);
         return addon;
     }
 
     public Addon fromDto(AddonUpdateDto addonUpdateDto, int id) {
         Addon addon = addonService.getById(id);
-        dtoUpdateObject(addonUpdateDto,addon);
+        dtoUpdateObject(addonUpdateDto, addon);
         return addon;
     }
 
@@ -60,7 +68,7 @@ public class ModelMapperAddon {
     //TODO method for update from admin after approving with additional info
     private void dtoUpdateObject(AddonUpdateDto addonUpdateDto, Addon addon) {
         addon.setDescription(addonUpdateDto.getDescription());
-        addon.setBinaryContentUrl(addonUpdateDto.getBinaryContentUrl());
+     //   addon.setBinaryContentUrl(addonUpdateDto.getBinaryContentUrl());
         Set<Tag> tags = new HashSet<>();
         for (Integer tag : addonUpdateDto.getTags()) {
             Tag tagToAdd = tagRepository.getById(tag);
@@ -69,19 +77,40 @@ public class ModelMapperAddon {
         addon.setTags(tags);
     }
 
-    public AddonUpdateDto toDto(Addon addon) {
-        AddonUpdateDto addonUpdateDto = new AddonUpdateDto();
-        addonUpdateDto.setName(addon.getName());
-        addonUpdateDto.setDescription(addon.getDescription());
-        addonUpdateDto.setOriginUrl(addon.getOriginUrl());
-        addonUpdateDto.setTargetIde(addon.getTargetIde().getId());
-        Set<Integer> tags = new HashSet<>();
-        for (Tag tag : addon.getTags()) {
-//            Tag tagToAdd = tagRepository.getById(tag.getId());
-            tags.add(tag.getId());
-        }
-        addonUpdateDto.setTags(tags);
-//        addonUpdateDto.setBinaryFile(addon.getBinaryFile());
-        return addonUpdateDto;
+    // =======================
+    // AddonDtoMvc
+    // =======================
+    public AddonDtoMvc toMvcDto(Addon addon) {
+        AddonDtoMvc addonDtoMvc = new AddonDtoMvc();
+        addonDtoMvc.setName(addon.getName());
+        addonDtoMvc.setDescription(addon.getDescription());
+        addonDtoMvc.setOriginUrl(addon.getOriginUrl());
+        addonDtoMvc.setTargetIde(addon.getTargetIde().getId());
+        addonDtoMvc.setTags(addon.getTags().stream().map(Tag::getId).collect(Collectors.toSet()));
+        return addonDtoMvc;
     }
-}
+
+    public Addon fromMvcDto(AddonDtoMvc addonDtoMvc, User creator) {
+        Addon addon = new Addon();
+        addon.setCreator(creator);
+        addon.setCreationDate(LocalDate.now());
+        addon.setApproved(false);
+        return populateAddonData(addon, addonDtoMvc);
+    }
+
+    public Addon fromMvcDto(AddonDtoMvc addonDtoMvc, int addonId) {
+        Addon addon = addonService.getById(addonId);
+        return populateAddonData(addon, addonDtoMvc);
+    }
+
+    private Addon populateAddonData(Addon addon, AddonDtoMvc addonDtoMvc){
+        addon.setName(addonDtoMvc.getName());
+        addon.setDescription(addonDtoMvc.getDescription());
+        addon.setOriginUrl(addonDtoMvc.getOriginUrl());
+        addon.setTargetIde(targetIdeService.getById(addonDtoMvc.getTargetIde()));
+        Set<Tag> tags = addonDtoMvc.getTags().stream().map(t->tagRepository.getById(t)).collect(Collectors.toSet());
+        addon.setTags(tags);
+        return addon;
+    }
+
+    }
