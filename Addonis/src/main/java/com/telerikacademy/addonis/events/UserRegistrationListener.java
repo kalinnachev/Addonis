a681765253fb7316ce7d1bbb2fff86ce26992398
kalinnachev <kalin.nachev@gmail.com>
2021-12-10 15:11:29 +0200

    Refactor sendConformationEmail

--- Addonis/src/main/java/com/telerikacademy/addonis/events/UserRegistrationListener.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/events/UserRegistrationListener.java
@@ -5,16 +5,9 @@ import com.telerikacademy.addonis.services.contracts.MailService;
 import com.telerikacademy.addonis.services.contracts.VerificationTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 @Component
@@ -22,19 +15,15 @@ public class UserRegistrationListener {
 
     private final VerificationTokenService verificationTokenService;
     private final MailService mailService;
-    private final JavaMailSender emailSender;
-
 
     @Autowired
-    public UserRegistrationListener(VerificationTokenService verificationTokenService, MailService mailService, JavaMailSender emailSender) {
+    public UserRegistrationListener(VerificationTokenService verificationTokenService, MailService mailService) {
         this.verificationTokenService = verificationTokenService;
         this.mailService = mailService;
-        this.emailSender = emailSender;
     }
 
-//    @Async
     @EventListener
-    public void sendConformationEmail(UserRegistrationCompleteEvent event) throws MessagingException, UnsupportedEncodingException {
+    public void sendConformationEmail(UserRegistrationCompleteEvent event) {
         String url = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .replacePath(null)
                 .build()
@@ -43,37 +32,17 @@ public class UserRegistrationListener {
         User user = event.getUser();
         String token = UUID.randomUUID().toString();
         verificationTokenService.createVerificationToken(user, token);
-        String object = "Please verify your registration";
-        String senderName = "Addon Team";
-        String mainContent = "<p>Dear" + user.getFirstName()+user.getLastName() +" ,</p>";
-        mainContent += "<p> Please click the link below to verify to your registration:</p>";
-
         String verityURL = url + "/auth/verify?token=" + token;
-        mainContent += "<h3><a =\"href=" + verityURL +"\" > VERIFY</a></h3>";
-        mainContent += "<p>Thank you<br>Addon Team</p>";
 
-        MimeMessage mailMessage = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mailMessage);
-        helper.setFrom("addonis.application@gmail.com",senderName);
-        helper.setTo(user.getEmail());
-        helper.setSubject(object);
-        helper.setText(mainContent,true);
-//        emailSender.send(mailMessage);
-//        SimpleMailMessage mailMessage = new SimpleMailMessage();
-//        mailMessage.setSubject("Addonis - Confirmation Email");
-//        mailMessage.setText(token);
-//        mailService.sendMail(user, mailMessage);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<p>Dear ");
+        stringBuilder.append(user.getFirstName() + " " + user.getLastName());
+        stringBuilder.append(" ,</p>");
 
-//        String recipientAddress = user.getEmail();
-//        String subject = "Registration Confirmation";
-//        String confirmationUrl
-//                = event.getAppUrl() + "/regitrationConfirm.html?token=" + token;
-//        String message = messages.getMessage("message.regSucc", null, event.getLocale());
-//
-//        SimpleMailMessage email = new SimpleMailMessage();
-//        email.setTo(recipientAddress);
-//        email.setSubject(subject);
-//        email.setText("http://localhost:8080" + confirmationUrl);
-//        mailSender.send(email);
+        stringBuilder.append("<p> Please click the link below to verify to your registration:</p>");
+        stringBuilder.append("<h3><a href=\"" + verityURL + "\" > VERIFY</a></h3>");
+        stringBuilder.append("<p>Thank you!<br>Addon Team</p>");
+
+        mailService.sendMail(user, "", stringBuilder.toString());
     }
 }
