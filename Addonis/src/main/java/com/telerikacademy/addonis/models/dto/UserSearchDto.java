5bc09aa6a641c8ca4cb4f3c9448c5edf7a90e0a1
Borislav Ignatov <borkata13@gmail.com>
2021-12-14 13:55:57 +0200

    Implement UserSearchDto

---
+++ Addonis/src/main/java/com/telerikacademy/addonis/models/dto/UserSearchDto.java
@@ -0,0 +1,33 @@
+package com.telerikacademy.addonis.models.dto;
+
+public class UserSearchDto {
+    private String username;
+
+    private String email;
+
+    private String phoneNumber;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
