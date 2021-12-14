de380721f379d48f640b365010172234552ba080
Borislav Ignatov <borkata13@gmail.com>
2021-12-14 16:29:48 +0200

    Implement search()

--- Addonis/src/main/java/com/telerikacademy/addonis/contollers/mvc/UserMVC.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/contollers/mvc/UserMVC.java
@@ -1,9 +1,7 @@
 package com.telerikacademy.addonis.contollers.mvc;
 
 import com.telerikacademy.addonis.exceptions.EntityNotFoundException;
-import com.telerikacademy.addonis.models.Addon;
 import com.telerikacademy.addonis.models.User;
-import com.telerikacademy.addonis.models.dto.SearchDto;
 import com.telerikacademy.addonis.models.dto.UserSearchDto;
 import com.telerikacademy.addonis.services.contracts.AddonService;
 import com.telerikacademy.addonis.services.contracts.UserService;
@@ -20,7 +18,7 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
-public class UserMVC extends BaseMvcController{
+public class UserMVC extends BaseMvcController {
 
     private final UserService userService;
     private final AddonService addonService;
@@ -36,38 +34,34 @@ public class UserMVC extends BaseMvcController{
 
     @GetMapping
     public String showAllUsers(Model model) {
-        model.addAttribute("users",userService.getAll());
+        model.addAttribute("users", userService.getAll());
         return "users";
     }
 
     @GetMapping("/{id}/block")
-    public String blockUser(@PathVariable int id, HttpSession session){
+    public String blockUser(@PathVariable int id, HttpSession session) {
         User user = getLoggedUser(session);
-        userService.block(id,user);
+        userService.block(id, user);
         return "redirect:/users/";
     }
 
     @GetMapping("/{id}/unblock")
-    public String unblockUser(@PathVariable int id, HttpSession session){
+    public String unblockUser(@PathVariable int id, HttpSession session) {
         User user = getLoggedUser(session);
-        userService.unblock(id,user);
+        userService.unblock(id, user);
         return "redirect:/users/";
     }
 
     @GetMapping("/{id}/delete")
-    public String deleteUser(@PathVariable int id, HttpSession session, Model model){
+    public String deleteUser(@PathVariable int id, HttpSession session, Model model) {
         User user = getLoggedUser(session);
-        try {
-            userService.delete(id, user);
-        } catch (IllegalArgumentException e){
-            model.addAttribute("msg", e.getMessage());
-            return "error";
-        }
+        userService.delete(id, user);
+
         return "redirect:/users/";
     }
 
     @GetMapping("/{id}/addons")
-    public String showAllAddonsOnUserPage(@PathVariable int id ,Model model, HttpSession session) {
+    public String showAllAddonsOnUserPage(@PathVariable int id, Model model, HttpSession session) {
         try {
             User user = getLoggedUser(session);
             User addonsUser = userService.getById(id);
@@ -79,5 +73,49 @@ public class UserMVC extends BaseMvcController{
         }
     }
 
+    @PostMapping("/search")
+    public String search(Model model, @ModelAttribute("userSearch") UserSearchDto userSearchDto, HttpSession session) {
+        User user = getLoggedUser(session);
+        List<User> usersList = extractSearchResult(userSearchDto, user);
+        model.addAttribute("users", usersList);
 
+        model.addAttribute("title", getSearchTitle(usersList.size()));
+        return "user-search";
+    }
+
+    private String getSearchTitle(int size) {
+        if (size == 1)
+            return "Search results (1 user):";
+        if (size == 0)
+            return "No users found";
+        return "Search results (" + size + " users):";
+    }
+
+    private List<User> extractSearchResult(UserSearchDto userSearchDto, User user) {
+        switch (userSearchDto.getSearchBy()) {
+            case 1:
+                return userService.search(
+                        Optional.ofNullable(userSearchDto.getParam()),
+                        Optional.empty(),
+                        Optional.empty(),
+                        user
+                );
+            case 2:
+                return userService.search(
+                        Optional.empty(),
+                        Optional.ofNullable(userSearchDto.getParam()),
+                        Optional.empty(),
+                        user
+                );
+            case 3:
+                return userService.search(
+                        Optional.empty(),
+                        Optional.empty(),
+                        Optional.ofNullable(userSearchDto.getParam()),
+                        user
+                );
+            default:
+                return userService.getAll();
+        }
+    }
 }
