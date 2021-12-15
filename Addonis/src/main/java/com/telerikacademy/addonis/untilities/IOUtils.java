cd9e729588d3ef20d7eebd5f9e56702084a161ca
kalinnachev <kalin.nachev@gmail.com>
2021-12-15 19:02:06 +0200

    Fix bug in file separator

--- Addonis/src/main/java/com/telerikacademy/addonis/untilities/IOUtils.java
+++ Addonis/src/main/java/com/telerikacademy/addonis/untilities/IOUtils.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 public class IOUtils {
 
     public static File convert(MultipartFile multipartFile) throws IOException {
-        File file = new File(System.getProperty("java.io.tmpdir") + multipartFile.getOriginalFilename());
+        File file = new File(System.getProperty("java.io.tmpdir")  +
+                File.separator + multipartFile.getOriginalFilename());
         multipartFile.transferTo(file);
         return file;
     }
